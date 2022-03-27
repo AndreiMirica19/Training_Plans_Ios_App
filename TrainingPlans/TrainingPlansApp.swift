cc47a06b0230337dab835a3a7ed56c674ab0d271
AndreiMirica19 <andreimirica12@gmail.com>
2022-03-27 08:25:20 +0300

    Initial Commit

---
+++ TrainingPlans/TrainingPlansApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingPlansApp.swift
+//  TrainingPlans
+//
+//  Created by Andrei Mirica on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrainingPlansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
